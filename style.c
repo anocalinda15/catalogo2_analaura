469fdc432cca62bb1bdc07ef5c18d82df2417740
anocalinda15 <ana.stecaneli@escola.pr.gov.br>
2025-08-21 11:51:32 -0300

    mudei o video do youtube

--- style.c
+++ style.c
@@ -1,24 +1,33 @@
 body {
-    color: black;
-    background: white;
-    margin: 0px;
+  color: black;
+  background: white;
+  margin: 0px;
+  font-family: "Chakra Petch", sans-serif;
 }
 
 header {
-    border-bottom: solid 2px rgba(228, 42, 219, 0.73);
-    padding: 20px;
-    font-size: 32px;
-    color: rgba(228, 42, 172, 0.75);
+  border-bottom: solid 2px rgba(222, 42, 228, 1);
+  padding: 20px;
+  font-size: 32px;
+  color: rgba(228, 42, 197, 1);
 }
 
 section {
-    background: rgba(0, 0, 0, 1);
-    padding-bottom: 80px;
-    padding-top: 80px;
-    display: flex;
-    justify-content: center;
+  background: rgba(15, 14, 15, 1);
+  padding-bottom: 80px;
+  padding-top: 80px;
+  display: flex;
+  justify-content: center;
 }
 
 .chamada-texto {
-    margin-right: 5%;
+  margin-right: 5%;
+}
+
+h1 {
+  font-size: 40px;
+}
+
+p {
+  font-size: 20px;
 }
