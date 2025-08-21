89b5cb70a63c3c7256e2f84560f2b6c3354ab302
anocalinda15 <ana.stecaneli@escola.pr.gov.br>
2025-08-21 11:49:20 -0300

    alterei cores

--- style.c
+++ style.c
@@ -1,6 +1,24 @@
+body {
+    color: black;
+    background: white;
+    margin: 0px;
+}
+
 header {
-    border-bottom: solid 2px rgba(231, 79, 155, 0.97);
+    border-bottom: solid 2px rgba(228, 42, 219, 0.73);
     padding: 20px;
     font-size: 32px;
-    color: rgba(255, 77, 216, 1);
+    color: rgba(228, 42, 172, 0.75);
+}
+
+section {
+    background: rgba(0, 0, 0, 1);
+    padding-bottom: 80px;
+    padding-top: 80px;
+    display: flex;
+    justify-content: center;
+}
+
+.chamada-texto {
+    margin-right: 5%;
 }
